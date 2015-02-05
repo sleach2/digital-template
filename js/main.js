91fe13d2209da59fa7487f677554f00a3c23eb94
sleach2 <sleach2@gmu.edu>
2015-02-05 01:51:43 -0500

    k

--- js/main.js
+++ js/main.js
@@ -51,8 +51,8 @@ window.onload = function() {
         cursors = game.input.keyboard.createCursorKeys();
         dogs = game.add.group();
         dogs.enableBody = true;
-        for (var i = 0; i < 12; i++){
-        var dog = dogs.create((i*(2*(i/3))) * 70, 0, 'dog');
+        for (var i = 0; i < Math.random(); i++){
+        var dog = dogs.create(i*Math.random(), 0, 'dog');
         dog.body.gravity.y = 150;
         }
         scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
