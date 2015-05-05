6cbe41782c2c5fddc7da4a99db5b0fa78a03d59b
sleach2 <sleach2@gmu.edu>
2015-05-05 15:30:45 -0400

    jm

--- js/main.js
+++ js/main.js
@@ -23,13 +23,17 @@ window.onload = function() {
     function create() {
         music=game.add.audio('boden');
         music.play();
+
         game.physics.startSystem(Phaser.Physics.ARCADE);
         game.add.sprite(0,0,'sky');
+
         platforms = game.add.group();
         platforms.enableBody = true;
+
         var ground = platforms.create(0, game.world.height - 64, 'ground');
         ground.scale.setTo(2, 2);
         ground.body.immovable = true;
+
         player = game.add.sprite(32, game.world.height - 150, 'dude');
         game.physics.arcade.enable(player);
         player.body.bounce.y = 0.2;
@@ -37,10 +41,13 @@ window.onload = function() {
         player.body.collideWorldBounds = true;
         player.animations.add('left', [0, 1, 2, 3], 10, true);
         player.animations.add('right', [5, 6, 7, 8], 10, true);
+
         scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
         cursors = game.input.keyboard.createCursorKeys();
+
         dogs = game.add.group();
         dogs.enableBody = true;
+
         game.time.events.repeat(Phaser.Timer.SECOND * 2, 10, abc, this);
         scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
     }
@@ -48,7 +55,7 @@ window.onload = function() {
     function abc(){
         count++;
         for (var i = 0; i < count; i++){
-            var dog = dogs.create(i*70, 0, 'dog');
+            var dog = dogs.create(game.rnd.integerInRange(0,game.world.width), 0, 'dog');
             dog.body.gravity.y = 75;
         }
     }
@@ -75,6 +82,6 @@ window.onload = function() {
     function collectDogs(player, dog) {
     dog.kill();
     score += 10;
-    scoreText.text = 'score: ' + score;
+    //scoreText.text = 'score: ' + score;
     }
 };
